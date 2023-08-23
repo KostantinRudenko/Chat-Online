dcab2823a1e7bb5870ffeb1873f0d204fa909e72
Rudenko_Kostya2010 <rudenkokostya10@gmail.com>
2023-08-23 14:15:57 +0300

    added status codes

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 # Status Codes
 
-STATUS_CODE = {400 : '400 Error. Server is full.'}
+STATUS_CODE = {100 : '100 : Wainting',
+               200 : '200 : OK',
+               400 : '400 Error : Server is full.'}
 
 #Server Datas
 
 HOST = '127.0.0.1'
 PORT = 8080
-
-#Client Datas
-
-CLIENT_COUNT = 3
+CLIENT_COUNT = 2
