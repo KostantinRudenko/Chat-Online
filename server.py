dd75dc074d0215cf6d8c7bbbbbac8fc0e53db332
Rudenko_Kostya2010 <rudenkokostya10@gmail.com>
2023-08-25 10:59:56 +0300

    Wrote server code. Needs fixes

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import select
+import threading
 from config import *
 
 class Server: # Main class
@@ -10,32 +12,50 @@ class Server: # Main class
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((HOST, PORT))
         self.server_socket.listen(CLIENT_COUNT)
+
         print("Server is ready!")
 
     def socket_server(self): # This function makes server works
-        
         while True:
-            client_data = self.client_connect()
-            client_socket = client_data[0]
-            while len(self.connected_clients) != 0:
-                data = client_socket.recv(1024)
-                self.broadcast_message(client_socket, data)
-
+            client_socket, addr = self.server_socket.accept()
+            self.client_data_saving(client_socket, addr)
+            self.broadcast_message(client_socket)
+            
+            client_thread = threading.Thread(target=self.broadcast_message, args=(self.broadcast_message, client_socket, addr))
+            client_thread.start()
+            
             if self.client_count > CLIENT_COUNT:
                 self.server_socket.send(STATUS_CODE[400])
                 break
         
-    def broadcast_message(self, client_socket, message): # This method sends massages to every connected client
-        for client_addr in self.connected_clients_addr:
-            client_socket.sendto(message, tuple(client_addr))
-    
-    def client_connect(self): # This method accepts clients which wants to connect
-        client_socket, addr = self.server_socket.accept()
+    def broadcast_message(self, client_socket): # This method sends massages to every connected client
+        client_thread = threading.Thread(target=self.broadcast_message, args=(client_socket,))
+        client_thread.start()
+        
+        try:
+            ready_to_read, _, _ = select.select([client_socket], [], [], 0)
+            if ready_to_read:
+                data = client_socket.recv(1024)
+                if not data:
+                    pass
+
+                for client in self.connected_clients:
+                    if client != client_socket:
+                        client.send(data)
+                    else:
+                        client.send(b'Massage was sent!')
+        except socket.error as e:
+            if e.errno == 10035:
+                pass
+
+    def client_data_saving(self, client_socket, addr): # This method accepts clients which wants to connect        
         self.client_count += 1
+        
         self.connected_clients.append(client_socket)
         self.connected_clients_addr.append(addr)
+        
         print(f'{addr} is connected')
-        result = [client_socket, addr]
+        result = client_socket
         return result
 
 if __name__ == "__main__":
