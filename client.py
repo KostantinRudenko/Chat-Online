4e1d29d13969408ae2af67d79fac5e85718a4095
Rudenko_Kostya2010 <rudenkokostya10@gmail.com>
2023-08-25 08:26:13 +0300

    Updated client a little

--- client.py
+++ client.py
@@ -4,15 +4,22 @@ from config import *
 
 class Client:
 
-    def socket_client(self):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((HOST, PORT))
+    def __init__(self) -> None:
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.connect((HOST, PORT))
         print('You are connected')
+
+
+    def socket_client(self):
         while True:
             massage = input("Massage: ")
-            client_socket.send(massage.encode())
-            data = client_socket.recv(1024).decode()
-            print(data)
+            self.server_socket.send(massage.encode())
+            data = self.server_socket.recv(1024).decode()
+            if not data:
+                continue
+            
+            elif data:
+                print(data)
 
 if __name__ == "__main__":
     client = Client()
