69ff9e5d402267e121b425594cf749380ac1d8f1
Rudenko_Kostya2010 <rudenkokostya10@gmail.com>
2023-09-08 21:14:50 +0300

    Fixed thread part

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import socket
 import select
 import threading
-from functools import partial
 from config import *
+from exceptions import *
 
 class Server: # Main class
-    def __init__(self, host, port) -> None: # This function run the server and save total info
+    def __init__(self, host, port) -> None:
+        '''
+        Runs the server. Saves total info
+        '''
         self.connected_clients = []
         self.connected_clients_addr=[]
         self.client_count = 0
@@ -15,50 +18,45 @@ class Server: # Main class
         self.server_socket.listen(CLIENT_COUNT)
 
         print("Server is ready!")
-
-    def socket_server(self): # This function makes server works
+        
+    def broadcast_message(self):
+        '''
+        Receives messages from clients and sends them to every client.
+        '''
         while True:
-            client_socket, addr = self.server_socket.accept()
-            self.client_data_saving(client_socket, addr)
-            self.broadcast_message(client_socket)
-            
-            client_thread = threading.Thread(target=partial(self.broadcast_message, client_socket))
-            client_thread.start()
-            
-            if self.client_count > CLIENT_COUNT:
-                self.server_socket.send(STATUS_CODE[400])
-                break
-        
-    def broadcast_message(self, client_socket): # This method sends massages to every connected client
-        client_thread = threading.Thread(target=self.broadcast_message, args=(client_socket,))
-        client_thread.start()
-        
-        try:
-            ready_to_read, _, _ = select.select([client_socket], [], [], 0)
-            if ready_to_read:
-                data = client_socket.recv(1024)
-                if not data:
+            try:
+                for client_socket in self.connected_clients:
+                    ready_to_read, _, _ = select.select([client_socket], [], [], 0)
+                    if ready_to_read:
+                        data = client_socket.recv(1024)
+                        if not data:
+                            pass
+                        client_socket.send(data)
+            except socket.error as e:
+                if e.errno == 10035:
                     pass
 
-                for client in self.connected_clients:
-                    if client != client_socket:
-                        client.send(data)
-                    else:
-                        client.send(b'Massage was sent!')
-        except socket.error as e:
-            if e.errno == 10035:
-                pass
+    def client_accepting(self) -> None:
+        '''
+        Accepts inccoming client's connections. Saves clients socket and address.
+        
+        '''
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            client_socket.send(b'You are connected')
 
-    def client_data_saving(self, client_socket, addr): # This method accepts clients which wants to connect        
-        self.client_count += 1
-        
-        self.connected_clients.append(client_socket)
-        self.connected_clients_addr.append(addr)
-        
-        print(f'{addr} is connected')
-        result = client_socket
-        return result
+            self.client_count += 1
+            
+            self.connected_clients.append(client_socket)
+            self.connected_clients_addr.append(addr)
+            
+            print(f'{addr} is connected')
 
 if __name__ == "__main__":
-    server = Server()
-    server.socket_server()
+    server = Server(HOST, PORT)
+    
+    accept_thread = threading.Thread(target=server.client_accepting, daemon=False)
+    broadcast_thread = threading.Thread(name='Sanya', target=server.broadcast_message, daemon=False)
+    
+    accept_thread.start()
+    broadcast_thread.start()
