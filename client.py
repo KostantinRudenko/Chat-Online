73760f8053a159bfc130402b37f3f20d2c4d0848
Rudenko_Kostya2010 <rudenkokostya10@gmail.com>
2023-09-19 21:39:57 +0300

    Tried to addept the code to asyncio library

--- client.py
+++ client.py
@@ -1,28 +1,43 @@
 import socket
 import threading
+import asyncio
 from config import *
 
 
-class Client:
+class Client(asyncio.Protocol):
 
     def __init__(self) -> None:
+        '''
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.connect((HOST, PORT))
+        '''
+        self.reader, self.writer = asyncio.open_connection(HOST, PORT)
 
-    def receive_message(self):
+    async def receive_message(self):
         '''
         Receives messages from server and return them.
         '''
-        while True:
-            data = self.server_socket.recv(1024).decode()
-            yield data
-    
-    def send_message(message : str, self):
+        try:
+            while True:
+                data = await self.reader.read(100)
+                if not data:
+                    pass
+                else:
+                    data.decode()
+                    for word in str(data).split():
+                        yield word
+
+        except asyncio.CancelledError:
+            pass
+
+    async def send_message(self):
         '''
         Sends message to server.
         '''
         while True:
-            self.server_socket.send(message.encode())
+            text = await self.reader.read(100).decode()
+            async for word in text.split():
+                yield word
 
 if __name__ == "__main__":
     client = Client()
--- server.py
+++ server.py
@@ -1,63 +1,53 @@
-import socket
+'''import socket'''
 import select
-import threading
+import asyncio
 
 from config import *
 from exceptions import *
 
-class Server: # Main class
-    def __init__(self, host, port) -> None:
+class Server(asyncio.Protocol): # Main class
+    '''
+    This class has functions for starting a new server and accepting clients.
+    '''
+
+    async def __init__(self) -> None:
         '''
         Runs the server. Saves total info
         '''
-        self.connected_clients = []
-        self.connected_clients_addr=[]
+        self.connected_clients = {}
         self.client_count = 0
 
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((host, port))
-        self.server_socket.listen(CLIENT_COUNT)
-        
     def broadcast_message(self):
         '''
         Receives messages from clients and sends them to every client.
         '''
         while True:
-            try:
-                for client_socket in self.connected_clients:
-                    ready_to_read, _, _ = select.select([client_socket], [], [], 0)
-                    if ready_to_read:
-                        data = client_socket.recv(1024)
-                        if not data:
-                            pass
-                        else:
-                            for client in self.connected_clients:
-                                if client == client_socket:
-                                    client.send(b'200 : OK')
-                                elif client != client_socket:
-                                    client_socket.send(data)
-            except socket.error as e:
-                if e.errno == 10035:
-                    pass
+            for client_socket in self.connected_clients:
+                ready_to_read, _, _ = select.select([client_socket], [], [], 0)
+                if ready_to_read:
+                    data = client_socket.recv(1024)
+                    if not data:
+                        pass
+                    else:
+                        for client in self.connected_clients:
+                            if client == client_socket:
+                                client.send(b'200 : OK')
+                            elif client != client_socket:
+                                client_socket.send(data)
 
-    def client_accepting(self) -> None:
+    def client_accepting(self, transport) -> None:
         '''
         Accepts inccoming client's connections. Saves clients socket and address.
         
         '''
+        client = transport.get_exctra_info('peername')
+        self.connected_clients[id(client)] = client
         while True:
-            client_socket, addr = self.server_socket.accept()
-            client_socket.send(b'You are connected')
+            pass
 
-            self.client_count += 1
-            
-            self.connected_clients.append(client_socket)
-            self.connected_clients_addr.append(addr)
+async def main():
+    server = await asyncio.start_server(Server, HOST, PORT)
+    await server.serve_forever()
 
 if __name__ == "__main__":
-    server = Server(HOST, PORT)
-    accept_thread = threading.Thread(target=server.client_accepting, daemon=False)
-    broadcast_thread = threading.Thread(name='Sanya', target=server.broadcast_message, daemon=False)
-        
-    accept_thread.start()
-    broadcast_thread.start()
+    asyncio.run(main())
